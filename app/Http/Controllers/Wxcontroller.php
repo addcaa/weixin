d83778c29b55e1df058e9b3fe259d4fe1dcc278c
addcaa <3152185832@qq.com>
2019-04-16 08:30:08 +0800

    语音更新

--- app/Http/Controllers/Wxcontroller.php
+++ app/Http/Controllers/Wxcontroller.php
@@ -29,7 +29,9 @@ class Wxcontroller extends Controller
         $MsgId=$data->MsgId;
         $PicUrl=$data->PicUrl;
         $u=$this->getUserInfo($openid);
-        $access=$this->AccessToren();
+        // print_r($u);die;
+        $client= new Client;
+        $access=$this->test();
         if($MsgType=="text"){
             // 下载用户文本
             $info=[
@@ -49,7 +51,7 @@ class Wxcontroller extends Controller
             </xml>
             ';
         }else if($MsgType=="image"){
-            $client= new Client;
+
             //获取临时素材
             $url="https://api.weixin.qq.com/cgi-bin/media/get?access_token=$access&media_id=$MediaId";
             // 下载用户图片
@@ -83,15 +85,34 @@ class Wxcontroller extends Controller
         }else if($MsgType=="voice"){
             //获取语音
             $url="https://api.weixin.qq.com/cgi-bin/media/get?access_token=$access&media_id=$MediaId";
-            $voictime=date('Y-m-d H:i:s');
-            $serfil=file_get_contents($url);
-            file_put_contents("/wwwroot/1809a/public/voice/$voictime.mp3",$serfil,FILE_APPEND);
+            $response=$client->get(new Uri($url));
+            // var_dump($response);die;
+            $headers=$response->getHeaders();//获取 相应 头信息
+            // print_r($headers);die;
+            $file_info=$headers['Content-disposition'][0]; //获取文件名
+            // echo $file_info;die;
+            $voice_name=rtrim(substr($file_info,-20),'"');
+            $res=Storage::put($voice_name,$response->getBody());
+            $info=[
+                'openid'=>$openid,
+                'm_name'=>$u['nickname'],
+                'm_sex'=>$u['sex'],
+                'm_headimg'=>$u['headimgurl'],
+                'm_time'=>$createTime,
+                'm_voice'=>"wwwroot/weixin/storage/app".$voice_name
+            ];
+            $arr=DB::table('message')->insert($info);
+            if($arr){
+                echo "成功";
+            }else{
+                echo "失败";
+            }
         }else if($MsgType=="video"){
             //视频接收
             $url="https://api.weixin.qq.com/cgi-bin/media/get?access_token=$access&media_id=$MediaId";
             $videotime=date('Y-m-d H:i:s');
             $resvideo=file_get_contents($url);
-            file_put_contents("/wwwroot/weixin/video/$videotime.mp4",$resvideo,FILE_APPEND);
+            file_put_contents("/wwwroot/1890a/video/$videotime.mp4",$resvideo,FILE_APPEND);
         }
         //判断登录
         if($event=='subscribe'){
@@ -145,14 +166,12 @@ class Wxcontroller extends Controller
         return $token;
     }
     public  function test(){
-        // Redis::set('11','44');
-        // dd(Redis::get('11'));
         $access_token=$this->AccessToren();
-        echo $access_token;
+        return $access_token;
     }
     /**用户信息 */
     public function getUserInfo($openid){
-        $url='https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$this->AccessToren().'&openid='.$openid.'&lang=zh_CN';
+        $url='https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$this->test().'&openid='.$openid.'&lang=zh_CN';
         //dd($url);
         $data=file_get_contents($url);
         $u=json_decode($data,true);
@@ -162,7 +181,7 @@ class Wxcontroller extends Controller
     public function createMenu(){
         //菜单接口
         // echo "111";die;
-        $url='https://api.weixin.qq.com/cgi-bin/menu/create?access_token='.$this->AccessToren();
+        $url='https://api.weixin.qq.com/cgi-bin/menu/create?access_token='.$this->test();
         //接口数据
         // echo $url;die;
         $post_arr=[
