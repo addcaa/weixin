cbb7edab3c6e8449efd6ab33ad65ef45bc8f429d
addcaa <3152185832@qq.com>
2019-04-24 13:56:35 +0800

    图片回复

--- resources/views/weixin/goodslist.blade.php
+++ resources/views/weixin/goodslist.blade.php
@@ -33,7 +33,8 @@
         signature: "{{$wxconfig['signature']}}",
         jsApiList: [
         'onMenuShareTimeline',
-        'updateAppMessageShareData'
+        'onMenuShareAppMessage',
+        'updateTimelineShareData'
         ]
         });
         wx.ready(function () {
@@ -45,16 +46,25 @@
                 // 用户点击了分享后执行的回调函数
                 },
             });
-            // 自定义“分享给朋友”及“分享到QQ”按钮的分享内容
-            wx.updateAppMessageShareData({
+            // // 自定义“分享给朋友”及“分享到QQ”按钮的分享内容
+            // wx.updateAppMessageShareData({
+            //     title: '电视', // 分享标题
+            //     desc: '大屏电视', // 分享描述
+            //     link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            //     imgUrl: '', // 分享图标
+            //     success: function () {
+            //     // 设置成功
+            //     }
+            // })
+            wx.onMenuShareAppMessage({
                 title: '电视', // 分享标题
                 desc: '大屏电视', // 分享描述
                 link: document.URL, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
                 imgUrl: 'http://mmbiz.qpic.cn/mmbiz_jpg/Hiak941wazMV8NXcT7cfIL1NMBf26bia8GOib2v1vO2qwQZgvR1vj9NibdFS7RBseaPPDRYpsqhJzTHBgpft2INGfA/0?wx_fmt=jpeg', // 分享图标
                 success: function () {
-                // 设置成功
+                // 用户点击了分享后执行的回调函数
                 }
-            })
+            });
         });
     </script>
 </body>
