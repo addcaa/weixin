19c41baa01f6a7b29392d1bbb5c2ba38e5960450
addcaa <3152185832@qq.com>
2019-04-24 11:45:45 +0800

    图片回复

--- resources/views/weixin/goodslist.blade.php
+++ resources/views/weixin/goodslist.blade.php
@@ -17,6 +17,7 @@
         signature: "{{$wxconfig['signature']}}",
         jsApiList: [
         'onMenuShareTimeline',
+        'onMenuShareAppMessage'
         ]
         });
         wx.ready(function () {
@@ -29,7 +30,18 @@
                 },
         });
         });
-    </script>
+        wx.ready(function(){
+            wx.onMenuShareAppMessage({
+                title: '电视', // 分享标题
+                desc: '大屏电视', // 分享描述
+                link: document.URL, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                imgUrl: 'http://mmbiz.qpic.cn/mmbiz_jpg/Hiak941wazMV8NXcT7cfIL1NMBf26bia8GOib2v1vO2qwQZgvR1vj9NibdFS7RBseaPPDRYpsqhJzTHBgpft2INGfA/0?wx_fmt=jpeg', // 分享图标
+                success: function () {
+                // 用户点击了分享后执行的回调函数
+                }
+            });
+        })
 
+    </script>
 </body>
 </html>
